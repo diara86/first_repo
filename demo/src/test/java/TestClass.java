810e0aa8bbce7c51ebad4d6e1f2d45992c4cde49
Daniela Vanovski <danielastaris@gmail.com>
2022-06-20 13:44:17 +0300

    Commit second pull request

--- demo/src/test/java/TestClass.java
+++ demo/src/test/java/TestClass.java
@@ -1,5 +1,8 @@
 public class TestClass {
     public static void main (String [] args){
+        System.out.println("Hello");
 
+        System.out.println("Let's make a pull request");
+        System.out.println("Let's make a pull request");
     }
 }
