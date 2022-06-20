81434b7bf3aae10756895faa1f11068ba654b994
Daniela Vanovski <danielastaris@gmail.com>
2022-06-20 13:23:37 +0300

    Add gitignore file

--- demo/src/test/java/TestClass.java
+++ demo/src/test/java/TestClass.java
@@ -1,5 +1,6 @@
 public class TestClass {
     public static void main (String [] args){
+        int v = 3;
         System.out.println("Hello");
 
         System.out.println("Let's make a pull request");
